4f9872bf43f081049c6a5cea61a490b25670c4ee
Hayden <haydenk@users.noreply.github.com>
2025-07-29 02:36:04 +0000

    add post method and CORS configuration

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from socket import create_connection
-from typing import Annotated
-from urllib.error import URLError
-from urllib.parse import urlparse
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import PlainTextResponse
+from pydantic import BaseModel
+
 
 app = FastAPI(docs_url=None, redoc_url=None)
+origins = ["www.isitup.sh", "haydenk.github.io", "*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+
+class DomainRequest(BaseModel):
+    host: str
+    port: int = 443
 
 
 def check_host(domain: str, port: int) -> bool:
@@ -20,15 +35,46 @@ def check_host(domain: str, port: int) -> bool:
 
 
 @app.get("/{domain}")
-def read_root(domain: str, port: Annotated[int, Query()] = 443):
+async def read_root(request: Request, domain: str):
     status: bool = False
+    host: str = domain
+    port: int = 443
+    if ";" in domain:
+        host, port = domain.split(";")
 
-    status = check_host(domain, port)
+    status = check_host(host, port)
     if port == 443 and not status:
-        status = check_host(domain, 80)    
+        status = check_host(host, 80)
+    
+    user_agent = request.headers.get("user-agent")
+    accept = request.headers.get("accept")
+
+    if accept == "text/plain" or "curl" in user_agent:
+        up_or_down: str = "up" if status else "down"
+        message: str = f"{host} appears to be {up_or_down}"
+        if ";" in domain:
+            message = f"{host}:{port} appears to be {up_or_down}"
+
+        return PlainTextResponse(message)
 
     return {
-        "domain": domain,
+        "host": host,
         "port": port,
+        "status": status,
+    }
+
+
+@app.post("/")
+async def post_root(body: DomainRequest):
+    status: bool = False
+
+    status = check_host(body.host, body.port)
+    if body.port == 443 and not status:
+        status = check_host(body.host, 80)    
+
+    return {
+        "host": body.host,
+        "port": body.port,
         "status": status
     }
+
